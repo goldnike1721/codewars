1bd6e750bdc058f7c79f7a7820e2c319750e63b5
Petro Rozumiei <164702625+goldnike1721@users.noreply.github.com>
2024-07-21 19:55:57 +0000

    21077

---
+++ 8 kyu/All Star Code Challenge #18.js
@@ -0,0 +1,66 @@
+--- Condition of the problem:
+
+/*
+Create a function that accepts a string and a single character, and returns an integer 
+of the count of occurrences the 2nd argument is found in the first one.
+If no occurrences can be found, a count of 0 should be returned.
+("Hello", 'o')  =>  1
+("Hello", 'l')  =>  2
+("", 'z')       =>  0
+Notes
+The first argument can be an empty string
+In languages with no distinct character data type, the second argument will be a string of length 1
+*/
+
+--- MY SOLUTION: ---
+clever: 2
+
+  function strCount(str, letter){  
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === letter) count++;
+    }
+    return count;
+  }
+
+
+  
+--- OTHER SOLUTIONS: ---
+
+1) clever: 1325
+
+  function strCount(str, letter){  
+    return str.split(letter).length-1
+  }
+
+2) clever: 112
+
+  function strCount(str, letter){  
+    return str.split('').filter(c => c == letter).length;
+  }
+
+3) clever: 36
+
+const strCount = (str, letter) => str.split(letter).length - 1;
+
+4) clever: 32
+
+  function strCount(str, letter) {  
+    return str.length - str.replace(new RegExp(letter, "gi"), '').length;
+  }
+
+5) clever: 31
+
+  function strCount(str, letter){  
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] == letter)
+        count++;
+    }
+    return count;
+  }
+
+6) clever: 18
+
+  const strCount = (str, letter) =>  
+    --str.split(letter).length;
---
+++ 8 kyu/Short Long Short.js
@@ -0,0 +1,80 @@
+--- Condition of the problem:
+
+/*
+Given 2 strings, a and b, return a string of the form short+long+short, with the shorter 
+string on the outside and the longer string on the inside. The strings will not be the same 
+length, but they may be empty ( zero length ).
+Hint for R users:
+The length of string is not always the same as the number of characters
+For example: (Input1, Input2) --> output
+("1", "22") --> "1221"
+("22", "1") --> "1221"
+ShortLongShort.solution("1", "22"); // returns "1221"
+ShortLongShort.solution("22", "1"); // returns "1221"
+*/
+
+--- MY SOLUTION: ---
+  clever: 0
+
+  function solution(a, b){
+    if (a.length < b.length) {
+      return a + b + a
+    }
+    else {
+      return b + a + b
+    }
+  }
+
+
+  
+--- OTHER SOLUTIONS: ---
+
+1) clever: 131
+
+  function solution(a, b) {
+    return a.length < b.length ? a + b + a : b + a + b
+  }
+
+2) clever: 71
+
+  // Using if...else, explicit block syntax
+  function solution(a, b) {
+    if (a.length < b.length) {
+        return a + b + a;
+    } else {
+        return b + a + b;
+    }
+  }
+
+  // Using if..., implicit else, compressed syntax
+  function solution(a, b) {
+    if (a.length < b.length) return a + b + a;
+    return b + a + b;
+  }
+
+  // Using ternary conditional operator
+  function solution(a, b) {
+    return (a.length < b.length) ? (a + b + a) : (b + a + b);
+  }
+
+  // ES6 arrow function
+  var solution = (a, b) =>
+    (a.length < b.length) ? (a + b + a) : (b + a + b);
+
+3) clever: 13
+
+  cfunction solution(a, b){
+    return (a.length > b.length) ? b + a + b : a + b + a;
+  }
+
+4) clever: 8
+
+  function solution(a, b){
+    const arr = []
+    if (a + b > 0){
+    if (a > b) arr.push(b), arr.push(a), arr.push(b)
+    if (b > a) arr.push(a), arr.push(b), arr.push(a)
+  } else if (a.length > b.length) arr.push(b), arr.push(a), arr.push(b)
+    else if (b.length > a.length) arr.push(a), arr.push(b), arr.push(a)
+      return arr.join("")
+  }
